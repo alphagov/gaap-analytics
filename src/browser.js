aa52bfb092669e49c1d07f90fa51f17f65ec628c
Jon Heslop <jon@jonheslop.com>
2018-01-10 14:30:17 +0000

    Revert "- moved browser root to own file"
    This reverts commit 54b9a1b298a468b2f644448e0eb98bb1fd7752e8.

--- src/browser.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-const eventTracking = require('./event-tracking')
-
-window.GAAP = window.GAAP || {}
-window.GAAP.analytics = eventTracking
--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 'use strict'
 const eventTracking = require('./event-tracking')
 
+// Add to window.GAAP if in browser context
+if (window) {
+  window.GAAP = window.GAAP || {}
+  window.GAAP.analytics = eventTracking
+}
+
 module.exports = eventTracking
