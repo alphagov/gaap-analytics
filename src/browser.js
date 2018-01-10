54b9a1b298a468b2f644448e0eb98bb1fd7752e8
maxcbc <maxcbc@live.com>
2018-01-10 14:05:30 +0000

    - moved browser root to own file

---
+++ src/browser.js
@@ -0,0 +1,5 @@
+'use strict'
+const eventTracking = require('./event-tracking')
+
+window.GAAP = window.GAAP || {}
+window.GAAP.analytics = eventTracking
--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 'use strict'
 const eventTracking = require('./event-tracking')
 
-// Add to window.GAAP if in browser context
-if (window) {
-  window.GAAP = window.GAAP || {}
-  window.GAAP.analytics = eventTracking
-}
-
 module.exports = eventTracking
